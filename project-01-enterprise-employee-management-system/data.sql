eb7c92c3c827f86773f61ada81cdc67eb173cf33
Alhanoof Alabdullah <alhanoofalabdullah@gmail.com>
2026-06-04 17:41:59 +0300

    Create data.sql

---
+++ project-01-enterprise-employee-management-system/data.sql
@@ -0,0 +1,8 @@
+INSERT INTO employees (full_name, email, department, salary, hire_date, status)
+VALUES ('Ahmad Mohammed', 'ahmad@example.com', 'IT', 15000, '2026-01-01', 'ACTIVE');
+
+INSERT INTO employees (full_name, email, department, salary, hire_date, status)
+VALUES ('Sara Ali', 'sara@example.com', 'Finance', 18000, '2026-01-10', 'ACTIVE');
+
+INSERT INTO employees (full_name, email, department, salary, hire_date, status)
+VALUES ('Khalid Hassan', 'khalid@example.com', 'HR', 12000, '2026-02-01', 'INACTIVE');
