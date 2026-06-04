5d5867755d705828fb94324b0e0a546ecc97bd3b
Alhanoof Alabdullah <alhanoofalabdullah@gmail.com>
2026-06-04 20:14:06 +0300

    Create Constants.java

---
+++ project-03-inventory-management-system/Constants.java
@@ -0,0 +1,13 @@
+package com.alhanoof.inventory.util;
+
+public class Constants {
+
+    public static final String STATUS_AVAILABLE = "AVAILABLE";
+
+    public static final String STATUS_LOW_STOCK = "LOW_STOCK";
+
+    public static final String STATUS_OUT_OF_STOCK = "OUT_OF_STOCK";
+
+    private Constants() {
+    }
+}
