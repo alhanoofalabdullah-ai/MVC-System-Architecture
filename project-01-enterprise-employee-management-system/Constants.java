c0b2401bc134bcd2d3b871297ca026a9854c27b5
Alhanoof Alabdullah <alhanoofalabdullah@gmail.com>
2026-06-04 17:35:17 +0300

    Create Constants.java

---
+++ project-01-enterprise-employee-management-system/Constants.java
@@ -0,0 +1,11 @@
+package com.alhanoof.employee.util;
+
+public class Constants {
+
+    public static final String STATUS_ACTIVE = "ACTIVE";
+
+    public static final String STATUS_INACTIVE = "INACTIVE";
+
+    private Constants() {
+    }
+}
