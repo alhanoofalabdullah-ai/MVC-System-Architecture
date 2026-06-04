3ba6dfb22d2cc75e65344939714525944217c0f4
Alhanoof Alabdullah <alhanoofalabdullah@gmail.com>
2026-06-04 20:14:44 +0300

    Create ValidationException.java

---
+++ project-03-inventory-management-system/ValidationException.java
@@ -0,0 +1,8 @@
+package com.alhanoof.inventory.exception;
+
+public class ValidationException extends RuntimeException {
+
+    public ValidationException(String message) {
+        super(message);
+    }
+}
