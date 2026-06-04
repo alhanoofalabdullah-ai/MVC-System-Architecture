dd16b21962abeb28ecd44f1bf62c573c1bccd284
Alhanoof Alabdullah <alhanoofalabdullah@gmail.com>
2026-06-05 01:41:35 +0300

    Create Constants.java

---
+++ project-04-sales-management-system/Constants.java
@@ -0,0 +1,13 @@
+package com.alhanoof.sales.util;
+
+public class Constants {
+
+    public static final String STATUS_PAID = "PAID";
+
+    public static final String STATUS_PENDING = "PENDING";
+
+    public static final String STATUS_CANCELLED = "CANCELLED";
+
+    private Constants() {
+    }
+}
