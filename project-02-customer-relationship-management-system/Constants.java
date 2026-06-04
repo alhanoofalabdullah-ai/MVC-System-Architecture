c320a75558f86db69683d858dd7297b5d855d327
Alhanoof Alabdullah <alhanoofalabdullah@gmail.com>
2026-06-04 19:48:27 +0300

    Create Constants.java

---
+++ project-02-customer-relationship-management-system/Constants.java
@@ -0,0 +1,17 @@
+package com.alhanoof.crm.util;
+
+public class Constants {
+
+    public static final String STATUS_ACTIVE = "ACTIVE";
+
+    public static final String STATUS_INACTIVE = "INACTIVE";
+
+    public static final String SEGMENT_STANDARD = "STANDARD";
+
+    public static final String SEGMENT_VIP = "VIP";
+
+    public static final String SEGMENT_ENTERPRISE = "ENTERPRISE";
+
+    private Constants() {
+    }
+}
