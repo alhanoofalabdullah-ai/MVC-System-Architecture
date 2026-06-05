3a82bfc503464559bb521241821cc5ddb97e763b
Alhanoof Alabdullah <alhanoofalabdullah@gmail.com>
2026-06-06 01:45:59 +0300

    Create Constants.java

---
+++ project-06-banking-management-system/Constants.java
@@ -0,0 +1,19 @@
+package com.alhanoof.banking.util;
+
+public class Constants {
+
+    public static final String TYPE_SAVINGS = "SAVINGS";
+
+    public static final String TYPE_CURRENT = "CURRENT";
+
+    public static final String TYPE_BUSINESS = "BUSINESS";
+
+    public static final String STATUS_ACTIVE = "ACTIVE";
+
+    public static final String STATUS_FROZEN = "FROZEN";
+
+    public static final String STATUS_CLOSED = "CLOSED";
+
+    private Constants() {
+    }
+}
