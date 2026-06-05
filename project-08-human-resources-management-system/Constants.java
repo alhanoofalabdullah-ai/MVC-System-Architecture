daa924cb2dbce817b09d5764da79eef9a98241ff
Alhanoof Alabdullah <alhanoofalabdullah@gmail.com>
2026-06-06 02:36:31 +0300

    Create Constants.java

---
+++ project-08-human-resources-management-system/Constants.java
@@ -0,0 +1,16 @@
+package com.alhanoof.hrms.util;
+
+public class Constants {
+
+    public static final String ACTIVE = "ACTIVE";
+    public static final String INACTIVE = "INACTIVE";
+    public static final String TERMINATED = "TERMINATED";
+
+    public static final String IT = "IT";
+    public static final String HR = "HR";
+    public static final String FINANCE = "FINANCE";
+    public static final String OPERATIONS = "OPERATIONS";
+
+    private Constants() {
+    }
+}
