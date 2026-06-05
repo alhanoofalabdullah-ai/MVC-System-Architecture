e42c21ae03551b88075b891c6678a14d2e069bde
Alhanoof Alabdullah <alhanoofalabdullah@gmail.com>
2026-06-06 01:46:25 +0300

    Create ValidationException.java

---
+++ project-06-banking-management-system/ValidationException.java
@@ -0,0 +1,8 @@
+package com.alhanoof.banking.exception;
+
+public class ValidationException extends RuntimeException {
+
+    public ValidationException(String message) {
+        super(message);
+    }
+}
