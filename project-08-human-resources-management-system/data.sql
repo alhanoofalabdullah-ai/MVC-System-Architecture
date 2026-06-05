e7f4fd5475c9a1773ddf2c2cb5b32fe48b28c918
Alhanoof Alabdullah <alhanoofalabdullah@gmail.com>
2026-06-06 02:37:27 +0300

    Create data.sql

---
+++ project-08-human-resources-management-system/data.sql
@@ -0,0 +1,32 @@
+INSERT INTO employees
+(full_name,email,department,position,salary,hire_date,status)
+VALUES
+('Alhanoof Alabdullah',
+'alhanoof@company.com',
+'IT',
+'Senior Systems Specialist',
+25000,
+'2026-01-01',
+'ACTIVE');
+
+INSERT INTO employees
+(full_name,email,department,position,salary,hire_date,status)
+VALUES
+('Ahmad Mohammed',
+'ahmad@company.com',
+'FINANCE',
+'Finance Analyst',
+18000,
+'2026-01-05',
+'ACTIVE');
+
+INSERT INTO employees
+(full_name,email,department,position,salary,hire_date,status)
+VALUES
+('Sara Ali',
+'sara@company.com',
+'HR',
+'HR Manager',
+22000,
+'2026-01-10',
+'ACTIVE');
