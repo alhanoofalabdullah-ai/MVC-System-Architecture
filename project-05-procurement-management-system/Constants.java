f36b3b0146af9c8f7eedde261ca73e4d3f460c59
Alhanoof Alabdullah <alhanoofalabdullah@gmail.com>
2026-06-06 01:27:44 +0300

    Create Constants.java

---
+++ project-05-procurement-management-system/Constants.java
@@ -0,0 +1,15 @@
+package com.alhanoof.procurement.util;
+
+public class Constants {
+
+    public static final String STATUS_APPROVED = "APPROVED";
+
+    public static final String STATUS_PENDING = "PENDING";
+
+    public static final String STATUS_REJECTED = "REJECTED";
+
+    public static final String STATUS_REVIEW_REQUIRED = "REVIEW_REQUIRED";
+
+    private Constants() {
+    }
+}
