9f91cec4102ad09414486e7eaa997122792fd9de
Alhanoof Alabdullah <alhanoofalabdullah@gmail.com>
2026-06-06 02:02:46 +0300

    Create Constants.java

---
+++ project-07-healthcare-management-system/Constants.java
@@ -0,0 +1,21 @@
+package com.alhanoof.healthcare.util;
+
+public class Constants {
+
+    public static final String STATUS_ACTIVE = "ACTIVE";
+
+    public static final String STATUS_DISCHARGED = "DISCHARGED";
+
+    public static final String STATUS_UNDER_TREATMENT = "UNDER_TREATMENT";
+
+    public static final String DEPARTMENT_CARDIOLOGY = "CARDIOLOGY";
+
+    public static final String DEPARTMENT_INTERNAL_MEDICINE = "INTERNAL_MEDICINE";
+
+    public static final String DEPARTMENT_DERMATOLOGY = "DERMATOLOGY";
+
+    public static final String DEPARTMENT_PEDIATRICS = "PEDIATRICS";
+
+    private Constants() {
+    }
+}
