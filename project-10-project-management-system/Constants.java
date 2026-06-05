d3452e5d10e024f1b86b207c1585194905d8f4d4
Alhanoof Alabdullah <alhanoofalabdullah@gmail.com>
2026-06-06 02:52:53 +0300

    Create Constants.java

---
+++ project-10-project-management-system/Constants.java
@@ -0,0 +1,16 @@
+package com.alhanoof.pms.util;
+
+public class Constants {
+
+    public static final String PLANNING = "PLANNING";
+    public static final String IN_PROGRESS = "IN_PROGRESS";
+    public static final String COMPLETED = "COMPLETED";
+    public static final String ON_HOLD = "ON_HOLD";
+
+    public static final String LOW = "LOW";
+    public static final String MEDIUM = "MEDIUM";
+    public static final String HIGH = "HIGH";
+
+    private Constants() {
+    }
+}
