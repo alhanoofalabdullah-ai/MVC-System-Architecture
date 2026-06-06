45c47b929322b880180cc12abca3f1c85e9e597f
Alhanoof Alabdullah <alhanoofalabdullah@gmail.com>
2026-06-06 05:06:17 +0300

    Create data.sql

---
+++ project-18-document-management-system/data.sql
@@ -0,0 +1,29 @@
+INSERT INTO documents
+(document_number,document_title,document_type,revision,status,created_date)
+VALUES
+('DOC-1001',
+'Wadi Safar IFC Drawing',
+'DRAWING',
+'A',
+'UNDER_REVIEW',
+'2026-01-01');
+
+INSERT INTO documents
+(document_number,document_title,document_type,revision,status,created_date)
+VALUES
+('DOC-1002',
+'Quality Audit Report',
+'REPORT',
+'B',
+'APPROVED',
+'2026-01-05');
+
+INSERT INTO documents
+(document_number,document_title,document_type,revision,status,created_date)
+VALUES
+('DOC-1003',
+'Technical Submittal',
+'SUBMITTAL',
+'0',
+'DRAFT',
+'2026-01-10');
