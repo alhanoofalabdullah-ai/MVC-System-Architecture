4fb7dc6521398e1869da50192851f1f2e3544b67
Alhanoof Alabdullah <alhanoofalabdullah@gmail.com>
2026-06-06 03:44:17 +0300

    Create Constants.java

---
+++ project-13-logistics-fleet-management-system/Constants.java
@@ -0,0 +1,12 @@
+package com.alhanoof.logistics.util;
+
+public class Constants {
+
+    public static final String PENDING = "PENDING";
+    public static final String IN_TRANSIT = "IN_TRANSIT";
+    public static final String DELIVERED = "DELIVERED";
+    public static final String CANCELLED = "CANCELLED";
+
+    private Constants() {
+    }
+}
