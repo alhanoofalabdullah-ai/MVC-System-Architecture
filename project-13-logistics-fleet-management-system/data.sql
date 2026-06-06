0ed45c60a8538d25b7ba38566eb9abc26831a1ed
Alhanoof Alabdullah <alhanoofalabdullah@gmail.com>
2026-06-06 03:49:43 +0300

    Create data.sql

---
+++ project-13-logistics-fleet-management-system/data.sql
@@ -0,0 +1,9 @@
+INSERT INTO shipments
+(shipment_number, origin, destination, driver_name, vehicle_number, shipment_cost, delivery_status, shipment_date)
+VALUES
+('SHP-1001', 'Riyadh', 'Jeddah', 'Ahmad Mohammed', 'TRK-5001', 4500, 'IN_TRANSIT', '2026-01-01');
+
+INSERT INTO shipments
+(shipment_number, origin, destination, driver_name, vehicle_number, shipment_cost, delivery_status, shipment_date)
+VALUES
+('SHP-1002', 'Dammam', 'Riyadh', 'Sara Ali', 'TRK-5002', 3200, 'DELIVERED', '2026-01-05');
