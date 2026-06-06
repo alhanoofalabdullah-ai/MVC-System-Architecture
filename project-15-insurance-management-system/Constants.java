d108c2aec198a04511e3f31591b909a39b9cd11b
Alhanoof Alabdullah <alhanoofalabdullah@gmail.com>
2026-06-06 04:18:12 +0300

    Create Constants.java

---
+++ project-15-insurance-management-system/Constants.java
@@ -0,0 +1,17 @@
+package com.alhanoof.insurance.util;
+
+public class Constants {
+
+    public static final String HEALTH = "HEALTH";
+    public static final String VEHICLE = "VEHICLE";
+    public static final String PROPERTY = "PROPERTY";
+    public static final String LIFE = "LIFE";
+
+    public static final String ACTIVE = "ACTIVE";
+    public static final String EXPIRED = "EXPIRED";
+    public static final String CANCELLED = "CANCELLED";
+    public static final String PENDING = "PENDING";
+
+    private Constants() {
+    }
+}
