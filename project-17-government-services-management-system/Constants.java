d120f6f432c860c4aa0185802d983db84e08bb6c
Alhanoof Alabdullah <alhanoofalabdullah@gmail.com>
2026-06-06 04:51:08 +0300

    Create Constants.java

---
+++ project-17-government-services-management-system/Constants.java
@@ -0,0 +1,12 @@
+package com.alhanoof.govservices.util;
+
+public class Constants {
+
+    public static final String PENDING = "PENDING";
+    public static final String APPROVED = "APPROVED";
+    public static final String REJECTED = "REJECTED";
+    public static final String IN_PROGRESS = "IN_PROGRESS";
+
+    private Constants() {
+    }
+}
