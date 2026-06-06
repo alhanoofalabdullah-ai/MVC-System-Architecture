a322863e12feac9511dbe3461e0659d0daddb439
Alhanoof Alabdullah <alhanoofalabdullah@gmail.com>
2026-06-06 04:03:55 +0300

    Create Constants.java

---
+++ project-14-hotel-reservation-system/Constants.java
@@ -0,0 +1,20 @@
+package com.alhanoof.hotel.util;
+
+public class Constants {
+
+    public static final String STANDARD = "STANDARD";
+    public static final String DELUXE = "DELUXE";
+    public static final String SUITE = "SUITE";
+
+    public static final String PAID = "PAID";
+    public static final String PENDING = "PENDING";
+    public static final String FAILED = "FAILED";
+
+    public static final String CONFIRMED = "CONFIRMED";
+    public static final String CHECKED_IN = "CHECKED_IN";
+    public static final String CHECKED_OUT = "CHECKED_OUT";
+    public static final String CANCELLED = "CANCELLED";
+
+    private Constants() {
+    }
+}
