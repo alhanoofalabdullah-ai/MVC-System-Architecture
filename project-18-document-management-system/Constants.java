3245331924937ec76f0bb29919e8338c8cded9aa
Alhanoof Alabdullah <alhanoofalabdullah@gmail.com>
2026-06-06 05:05:15 +0300

    Create Constants.java

---
+++ project-18-document-management-system/Constants.java
@@ -0,0 +1,17 @@
+package com.alhanoof.dms.util;
+
+public class Constants {
+
+    public static final String DRAWING = "DRAWING";
+    public static final String SPECIFICATION = "SPECIFICATION";
+    public static final String REPORT = "REPORT";
+    public static final String SUBMITTAL = "SUBMITTAL";
+
+    public static final String DRAFT = "DRAFT";
+    public static final String UNDER_REVIEW = "UNDER_REVIEW";
+    public static final String APPROVED = "APPROVED";
+    public static final String REJECTED = "REJECTED";
+
+    private Constants() {
+    }
+}
