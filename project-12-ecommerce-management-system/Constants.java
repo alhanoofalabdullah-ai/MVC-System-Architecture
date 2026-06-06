caf4c03b0e48fc09ff2f8120951f71e8b76b00b9
Alhanoof Alabdullah <alhanoofalabdullah@gmail.com>
2026-06-06 03:21:02 +0300

    Create Constants.java

---
+++ project-12-ecommerce-management-system/Constants.java
@@ -0,0 +1,16 @@
+package com.alhanoof.ecommerce.util;
+
+public class Constants {
+
+    public static final String PAID = "PAID";
+    public static final String PENDING = "PENDING";
+    public static final String FAILED = "FAILED";
+
+    public static final String CONFIRMED = "CONFIRMED";
+    public static final String SHIPPED = "SHIPPED";
+    public static final String DELIVERED = "DELIVERED";
+    public static final String CANCELLED = "CANCELLED";
+
+    private Constants() {
+    }
+}
