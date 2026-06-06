f61f199e151f558b37e9b502b578146e0fa42c87
Alhanoof Alabdullah <alhanoofalabdullah@gmail.com>
2026-06-06 03:09:55 +0300

    Create Constants.java

---
+++ project-11-learning-management-system/Constants.java
@@ -0,0 +1,11 @@
+package com.alhanoof.lms.util;
+
+public class Constants {
+
+    public static final String ACTIVE = "ACTIVE";
+    public static final String COMPLETED = "COMPLETED";
+    public static final String DROPPED = "DROPPED";
+
+    private Constants() {
+    }
+}
