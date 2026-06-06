67c4da5a4d84a98c69e42a7406f8350c6c3fbcd8
Alhanoof Alabdullah <alhanoofalabdullah@gmail.com>
2026-06-06 05:12:03 +0300

    Create Constants.java

---
+++ project-19-audit-compliance-management-system/Constants.java
@@ -0,0 +1,16 @@
+package com.alhanoof.audit.util;
+
+public class Constants {
+
+    public static final String COMPLIANCE = "COMPLIANCE";
+    public static final String GOVERNANCE = "GOVERNANCE";
+    public static final String QUALITY = "QUALITY";
+    public static final String RISK = "RISK";
+
+    public static final String OPEN = "OPEN";
+    public static final String CLOSED = "CLOSED";
+    public static final String IN_PROGRESS = "IN_PROGRESS";
+
+    private Constants() {
+    }
+}
