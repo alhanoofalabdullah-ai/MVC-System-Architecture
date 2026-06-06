21a6a5cea10fd750ee8e9959b1f037a0fdbb8634
Alhanoof Alabdullah <alhanoofalabdullah@gmail.com>
2026-06-06 05:20:41 +0300

    Create Constants.java

---
+++ project-20-enterprise-risk-management-system/Constants.java
@@ -0,0 +1,17 @@
+package com.alhanoof.erm.util;
+
+public class Constants {
+
+    public static final String CYBERSECURITY = "CYBERSECURITY";
+    public static final String OPERATIONAL = "OPERATIONAL";
+    public static final String FINANCIAL = "FINANCIAL";
+    public static final String COMPLIANCE = "COMPLIANCE";
+    public static final String STRATEGIC = "STRATEGIC";
+
+    public static final String OPEN = "OPEN";
+    public static final String MITIGATED = "MITIGATED";
+    public static final String CLOSED = "CLOSED";
+
+    private Constants() {
+    }
+}
