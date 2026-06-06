b9a07c1b12f6c739353a125c083f6a72f8259d62
Alhanoof Alabdullah <alhanoofalabdullah@gmail.com>
2026-06-06 03:26:01 +0300

    Create data.sql

---
+++ project-12-ecommerce-management-system/data.sql
@@ -0,0 +1,9 @@
+INSERT INTO orders
+(customer_name, product_name, category, quantity, unit_price, total_amount, payment_status, order_status, order_date)
+VALUES
+('Ahmad Mohammed', 'Laptop', 'ELECTRONICS', 2, 3500, 7000, 'PAID', 'CONFIRMED', '2026-01-01');
+
+INSERT INTO orders
+(customer_name, product_name, category, quantity, unit_price, total_amount, payment_status, order_status, order_date)
+VALUES
+('Sara Ali', 'Phone', 'ELECTRONICS', 1, 4200, 4200, 'PENDING', 'SHIPPED', '2026-01-05');
