d60e986246d00861cfa8ab0a5c7e74b48ef6e585
Alhanoof Alabdullah <alhanoofalabdullah@gmail.com>
2026-06-06 04:39:10 +0300

    Create Constants.java

---
+++ project-16-asset-management-system/Constants.java
@@ -0,0 +1,17 @@
+package com.alhanoof.asset.util;
+
+public class Constants {
+
+    public static final String LAPTOP = "LAPTOP";
+    public static final String VEHICLE = "VEHICLE";
+    public static final String EQUIPMENT = "EQUIPMENT";
+    public static final String SOFTWARE = "SOFTWARE";
+
+    public static final String ACTIVE = "ACTIVE";
+    public static final String UNDER_MAINTENANCE = "UNDER_MAINTENANCE";
+    public static final String RETIRED = "RETIRED";
+    public static final String LOST = "LOST";
+
+    private Constants() {
+    }
+}
